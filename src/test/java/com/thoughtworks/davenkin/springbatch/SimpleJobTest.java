91e2fba92efbd52b487969dedeff687ea2829826
Davenkin <davenkin@163.com>
2012-10-14 21:14:40 +0800

    add test for single tasklet step

---
+++ src/test/java/com/thoughtworks/davenkin/springbatch/SimpleJobTest.java
@@ -0,0 +1,53 @@
+package com.thoughtworks.davenkin.springbatch;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import static org.junit.Assert.assertEquals;
+
+@ContextConfiguration(locations = {
+        "classpath:applicationContext.xml",
+        "classpath:testContext.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SimpleJobTest
+{
+    /** JobLauncherTestUtils Bean. */
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private DataSource dataSource;
+
+    /** Launch Test. */
+    @Test
+    public void launchJob() throws Exception {
+        // launch the job, the utils provide the job with a unique parameter
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+
+        // assert job run status
+        assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
+        Connection connection = dataSource.getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from BATCH_JOB_EXECUTION");
+        while(resultSet.next())
+        {
+            String anInt = resultSet.getString(1);
+            System.out.println(anInt);
+            String str = resultSet.getString("START_TIME");
+            System.out.println(str);
+
+        }
+    }
+}
--- src/test/java/com/thoughtworks/davenkin/springbatch/SimpleTaskletStepTest.java
+++ src/test/java/com/thoughtworks/davenkin/springbatch/SimpleTaskletStepTest.java
@@ -1,53 +1,37 @@
 package com.thoughtworks.davenkin.springbatch;
 
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.batch.core.BatchStatus;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.test.JobLauncherTestUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
 
-@ContextConfiguration(locations = {
-        "classpath:applicationContext.xml",
-        "classpath:testContext.xml"})
-@RunWith(SpringJUnit4ClassRunner.class)
 public class SimpleTaskletStepTest
 {
-    /** JobLauncherTestUtils Bean. */
-    @Autowired
-    private JobLauncherTestUtils jobLauncherTestUtils;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final Tasklet tasklet = new SimpleTaskletStep();
 
-    @Autowired
-    private DataSource dataSource;
-
-    /** Launch Test. */
     @Test
-    public void launchJob() throws Exception {
-        // launch the job, the utils provide the job with a unique parameter
-        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+    public void testExecute() throws Exception
+    {
+        assertEquals(RepeatStatus.FINISHED, tasklet.execute(null, null));
+        assertEquals("Hello World!", outContent.toString());
+    }
 
-        // assert job run status
-        assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
-        Connection connection = dataSource.getConnection();
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("select * from BATCH_JOB_EXECUTION");
-        while(resultSet.next())
-        {
-            String anInt = resultSet.getString(1);
-            System.out.println(anInt);
-            String str = resultSet.getString("START_TIME");
-            System.out.println(str);
+    @Before
+    public void setUpStreams()
+    {
+        System.setOut(new PrintStream(outContent));
+    }
 
-        }
+    @After
+    public void cleanUpStreams()
+    {
+        System.setOut(null);
     }
 }
