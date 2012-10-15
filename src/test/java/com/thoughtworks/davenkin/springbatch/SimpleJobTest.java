901f3340841859079e96ba66936ffba7da0c9947
Davenkin <davenkin@163.com>
2012-10-15 23:29:01 +0800

    added custom paramterizd job

--- src/test/java/com/thoughtworks/davenkin/springbatch/SimpleJobTest.java
+++ src/test/java/com/thoughtworks/davenkin/springbatch/SimpleJobTest.java
@@ -2,16 +2,17 @@ package com.thoughtworks.davenkin.springbatch;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.batch.core.BatchStatus;
-import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
 
-import static org.junit.Assert.assertEquals;
+import static junit.framework.Assert.assertEquals;
 
 @ContextConfiguration(locations = {
         "classpath:applicationContext.xml",
@@ -25,11 +26,28 @@ public class SimpleJobTest
     @Autowired
     private DataSource dataSource;
 
+    @Autowired
+    private Job simpleTaskletStepJob;
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
     @Test
     public void launchJob() throws Exception
     {
         JobExecution jobExecution = jobLauncherTestUtils.launchJob();
 
         assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
+    }
+
+    @Test
+    public void testJobByParameter() throws Exception
+    {
+        HashMap<String, JobParameter> parameters = new HashMap<String, JobParameter>();
+//        parameters.put("myParameter", new JobParameter("parameter1"));
+        parameters.put("myParameter", new JobParameter(System.currentTimeMillis()));
+        JobParameters jobParameters = new JobParameters(parameters);
+        jobLauncher.run(simpleTaskletStepJob, jobParameters);
+
     }
 }
